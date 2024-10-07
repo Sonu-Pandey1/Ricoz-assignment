646b2c3eface877374eaa8355869552bca03a48b
Sonu-Pandey1 <sonupandeybaldeo2003@gmail.com>
2024-10-07 09:44:59 +0530

    connect MongoDb

--- backend/conf/db/dbConfig.js
+++ backend/conf/db/dbConfig.js
@@ -1,3 +1,18 @@
 
+const mongoose = require("mongoose");
+
+const dbConnect = async ()=>{
+
+    try {
+        const connection = await  mongoose.connect(process.env.MONGODB_URI);
+        console.log("mongoDb Connected Successfully.")
+        
+    } catch (error) {
+        console.log("mongoDb Connection Error:",error)
+        
+    }
+}
+
+module.exports = dbConnect;
 
 
